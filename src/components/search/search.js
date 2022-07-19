7fb66ba46297c4a4badb3648583370b9f0f9ea52
Karla Abad <kabad1014@gmail.com>
2022-07-19 15:01:01 -0400

    Current weather component css

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,13 +1,34 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  //minPopulation is a parameter from the the API that allows you to fetch cities that meet the condition, in order to reduce the number of results the user sees.
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json()) //When using fetch, you don't get information back as json, thus first we need to make it into json format.
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
-  }
+  };
 
   return (
     <AsyncPaginate
@@ -15,6 +36,7 @@ const Search = ({onSearchChange}) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   );
 };
