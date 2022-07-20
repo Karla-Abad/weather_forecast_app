1da2eedcfcb4c9559aa41b76c39b19b4a34e9fdd
Karla Abad <kabad1014@gmail.com>
2022-07-20 09:56:11 -0400

    Forecast Component - CSS and Fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 import {WEATHER_API_KEY, WEATHER_API_URL} from "./api"
 import { useState } from 'react';
+import Forecast from './components/forecast/forecast';
+
 
 function App() {
 
@@ -32,7 +34,8 @@ function App() {
   return (
     <div className="container">
         <Search onSearchChange={handleOnSearchChange} />
-        {currentWeather&&<CurrentWeather data={currentWeather} />}
+        {currentWeather && <CurrentWeather data={currentWeather} />}
+        {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
